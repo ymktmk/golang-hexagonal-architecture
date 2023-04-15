b04df3075616d6bd70abd9fcbac7e45ce9fe8946
ymktmk <ymktmk.tt@gmail.com>
2023-04-15 20:38:46 +0900

    [fix]

--- migration/seed_tables.sql
+++ migration/seed_tables.sql
@@ -1,9 +1,9 @@
 INSERT INTO users(id, name, email) VALUES
-      (1,'xxxxxxxxxx1','influencer1@gmail.com'),
-      (2,'xxxxxxxxxx2','influencer2@gmail.com'),
-      (3,'xxxxxxxxxx3','influencer3@gmail.com'),
-      (4,'xxxxxxxxxx4','influencer4@gmail.com'),
-      (5,'xxxxxxxxxx5','company1@gmail.com'),
-      (6,'xxxxxxxxxx6','company2@gmail.com'),
-      (7,'xxxxxxxxxx7','company3@gmail.com'),
-      (8,'xxxxxxxxxx8','company4@gmail.com');
+      (1,'xxxxxxxxxx1','aaa@gmail.com'),
+      (2,'xxxxxxxxxx2','bbb@gmail.com'),
+      (3,'xxxxxxxxxx3','ccc@gmail.com'),
+      (4,'xxxxxxxxxx4','ddd@gmail.com'),
+      (5,'xxxxxxxxxx5','eee@gmail.com'),
+      (6,'xxxxxxxxxx6','fff@gmail.com'),
+      (7,'xxxxxxxxxx7','ggg@gmail.com'),
+      (8,'xxxxxxxxxx8','hhh@gmail.com');
