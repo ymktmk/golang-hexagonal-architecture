6ca479dbe3dea571679c2450ba9d6350c2646d2f
ymktmk <73768462+ymktmk@users.noreply.github.com>
2023-04-17 01:29:48 +0900

    [fix]

--- app/adapter/impl/service/getting_user_service_impl.go
+++ app/adapter/impl/service/getting_user_service_impl.go
@@ -8,15 +8,15 @@ import (
 	"github.com/ymktmk/golang-hexagonal-architecture/app/domain/service"
 )
 
-type GettingUserServiceImpl struct {
+type gettingUserServiceImpl struct {
 	userRepository repository.UserRepository
 }
 
 func NewGettingUserService(ur repository.UserRepository) service.GettingUserService {
-	return &GettingUserServiceImpl{userRepository: ur}
+	return &gettingUserServiceImpl{userRepository: ur}
 }
 
-func (s *GettingUserServiceImpl) Index() ([]model.User, error) {
+func (s *gettingUserServiceImpl) Index() ([]model.User, error) {
 	users, err := s.userRepository.FindAll()
 	if err != nil {
 		log.Println(err)
